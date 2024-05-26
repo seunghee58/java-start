9e1b6dc223454cbc5f8ecd200a850c707858c843
seunghee58 <zzuggumee@gmail.com>
2024-05-26 21:53:06 +0900

    변수 스터디

---
+++ src/variable/Var4.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        // 변수는 하나씩 선언도 가능하고
+        int a;
+        int b;
+        // 한번에 여러 변수 선언도 가능하다
+        int c,d;
+    }
+}
---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,12 @@
+package variable.ex;
+
+public class VarEx3 {
+
+    public static void main(String[] args) {
+        long l = 100000000L;
+        System.out.println(l);
+
+        boolean b = true;
+        System.out.println(b);
+    }
+}
