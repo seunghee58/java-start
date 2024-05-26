31c5684f64ade7ca2cb2f10a9aaeabf4f630f38e
seunghee58 <zzuggumee@gmail.com>
2024-05-26 20:37:22 +0900

    Initial commit

---
+++ src/CommentJava.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java1"); //Hello Java1을 출력합니다. 한 줄 주석 - 부분 적용
+        //System.out.println("Hello Java2"); 한 줄 주석 - 전체 라인 적용
+
+        /* 여러 줄 주석
+        System.out.println("Hello Java3");
+        System.out.println("Hello Java4");
+        */
+    }
+}
---
+++ src/HelloJava2.java
@@ -0,0 +1,8 @@
+public class HelloJava2 {
+
+    public static void main(String[] args){
+        System.out.println("Hello JAVA1");
+        System.out.println("Hello JAVA2");
+        System.out.println("Hello JAVA3");
+    }
+}
