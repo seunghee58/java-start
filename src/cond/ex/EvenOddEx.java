94e158bbfbcf00bad71f91176cacb77914c8aae6
seunghee58 <zzuggumee@gmail.com>
2024-05-30 02:32:10 +0900

    조건문 연습문제 풀이

--- src/cond/ex/EvenOddEx.java
+++ src/cond/ex/EvenOddEx.java
@@ -5,7 +5,11 @@ public class EvenOddEx {
         /*
         정수 x 가 주어지면 x 가 짝수이면 "짝수"를, x 가 홀수이면 "홀수"를 출력하는 프로그램을 작성하자
         삼항 연산자를 사용해야 한다.
+        출력 예시 - x = 2, 짝수
         */
+        int x = 2;
+        String result = (x % 2 == 0) ? "짝수" : "홀수";
+        System.out.println("x = " + x + " , " + result);
 
     }
 }
